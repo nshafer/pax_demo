1cb7e5c5973262269bb73a3f0f388c0ce5450bf4
Nathan Shafer <nate@lotech.org>
2023-09-01 14:54:50 -0700

    silly benchmarks (converting kw to map is twice as slow, but still super fast... stop prematurely optimizing....)

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule PaxDemo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:benchee, "~> 1.0", only: :dev}
     ] ++ pax_deps()
   end
 
