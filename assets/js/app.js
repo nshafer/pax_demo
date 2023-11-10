e7ffb2107ab14570a4ac4432386cb7f500a9b93f
Nathan Shafer <nate@lotech.org>
2023-11-10 11:01:52 -0700

    Disable bs js

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,4 +40,4 @@ liveSocket.connect()
 window.liveSocket = liveSocket
 
 // Import Bootstrap JS for interactive features
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
+//import '../node_modules/bootstrap/dist/js/bootstrap.js';
