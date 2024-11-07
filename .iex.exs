f1339652df72f45189a8f1c6cccd81ea3863099c
Nathan Shafer <nate@lotech.org>
2024-11-07 15:38:56 -0700

    iex shortcut aliases

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+import Ecto.Query, only: [from: 1, from: 2]
+
+alias PaxDemo.Repo
+
+alias PaxDemo.Library
+alias PaxDemo.Library.Author
+alias PaxDemo.Library.Book
+alias PaxDemo.Library.BookSubject
+alias PaxDemo.Library.Classification
+alias PaxDemo.Library.Language
+alias PaxDemo.Library.Subject
