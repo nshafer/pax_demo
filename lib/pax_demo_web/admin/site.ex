2c84caa0dd04ed6b6d5e2614d14b6bb17d44e827
Nathan Shafer <nate@lotech.org>
2024-11-09 11:47:53 -0700

    Update admin config

--- lib/pax_demo_web/admin/site.ex
+++ lib/pax_demo_web/admin/site.ex
@@ -1,12 +1,12 @@
 defmodule PaxDemoWeb.Admin.Site do
   use Pax.Admin.Site, router: PaxDemoWeb.Router
 
-  config title: "Pax Admin"
+  config title: "Pax Demo Admin"
 
-  resource :books, PaxDemoWeb.Admin.BookResource
+  resource :books, PaxDemoWeb.Admin.BookResource, label: "Book List"
   resource :authors, PaxDemoWeb.Admin.AuthorResource
 
-  section :metadata do
+  section :metadata, label: "Metadata" do
     resource :subjects, PaxDemoWeb.Admin.SubjectResource
     resource :classifications, PaxDemoWeb.Admin.ClassificationResource
     resource :languages, PaxDemoWeb.Admin.LanguageResource
