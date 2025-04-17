93cea97d5fa928b59ef360aa751179eb688dd818
Nathan Shafer <nate@lotech.org>
2025-04-17 14:39:04 -0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule PaxDemo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.4"},
-      {:dns_cluster, "~> 0.1.1"},
+      {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.6"},
       pax_dep()
     ]
